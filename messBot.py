22e187a59563ff33239189ebb58946e3a471832b
JuniorJPDJ <git@juniorjpdj.pl>
2019-04-13 07:05:37 +0200

    .

--- messBot.py
+++ messBot.py
@@ -6,7 +6,7 @@ Created on Fri Apr 12 20:41:32 2019
 @author: Papito
 """
 
-#Python libraries that we need to import for our bot
+# Python libraries that we need to import for our bot
 import random
 from flask import Flask, request
 from pymessenger.bot import Bot
@@ -18,7 +18,7 @@ VERIFY_TOKEN = 'VERIFY_TOKEN'
 bot = Bot(ACCESS_TOKEN)
 es = Elasticsearch()
 
-#We will receive messages that Facebook sends our bot at this endpoint 
+# We will receive messages that Facebook sends our bot at this endpoint
 @app.route("/", methods=['GET', 'POST'])
 def receive_message():
     if request.method == 'GET':
@@ -68,39 +68,19 @@ def receive_message():
 
 
 def verify_fb_token(token_sent):
-    #take token sent by facebook and verify it matches the verify token you sent
-    #if they match, allow the request, else return an error 
+    # take token sent by facebook and verify it matches the verify token you sent
+    # if they match, allow the request, else return an error
     if token_sent == VERIFY_TOKEN:
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
 
 
-#chooses a random message to send to the user
-def get_message():
-    sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
-    # return selected item to the user
-    return random.choice(sample_responses)
-
-#uses PyMessenger to send response to user
+# uses PyMessenger to send response to user
 def send_message(recipient_id, response):
-    #sends user the text message provided via input response parameter
+    # sends user the text message provided via input response parameter
     bot.send_text_message(recipient_id, response)
     return "success"
 
+
 if __name__ == "__main__":
     app.run()
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
