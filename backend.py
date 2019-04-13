c7b33e57ea710c3554f8890f598d97123075725e
JuniorJPDJ <git@juniorjpdj.pl>
2019-04-13 02:13:48 +0200

    Early API version

---
+++ backend.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, session, abort
+from flask_restful import Resource, Api, reqparse
+from flask_session import Session
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SECRET_KEY'] = '_5#y2L"F4Q8zsdhgfdh'
+app.config['SESSION_TYPE'] = "filesystem"
+Session(app)
+
+
+class Chat(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('message')
+
+    def get(self, last_message_id):
+        if "messages" in session:
+            return session["messages"][last_message_id:]
+        else:
+            abort(401)
+
+    def put(self, last_message_id):
+        if "messages" in session:
+            args = self.parser.parse_args()
+            if args["message"] is None:
+                abort(400)
+            session["messages"].append(args['message'])
+        return self.get(last_message_id)
+
+
+class Login(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('login')
+    parser.add_argument('password')
+
+    def post(self):
+        args = self.parser.parse_args()
+        if args['login'] == "admin" and args['password'] == "admin":
+            # init session
+            session['messages'] = []
+            return {"message": "Logged in successfully"}
+        else:
+            abort(401)
+
+
+api.add_resource(Chat, '/api/chat/v1.0/message/<int:last_message_id>')
+api.add_resource(Login, '/api/chat/v1.0/login')
+
+if __name__ == '__main__':
+    app.run(debug=True)
